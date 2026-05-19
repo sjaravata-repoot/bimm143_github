68876ed96f07aa5ac9f18d6379b2bc0f3b205bdc
Sofia Jaravata <sjaravat@ucsd.edu>
2026-05-19 12:43:28 -0700

    Add class 4-7

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class04: Intro to R"
+#' author: "Sofia Jaravata (PID:A19160915)"
+#' date: "April 9th, 2026"
+#' ---
+
+x <- 1:50
+plot(x,sin(x),type = "l", col = "salmon", lwd = 3)
+
